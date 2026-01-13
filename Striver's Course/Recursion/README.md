e7c33ad3492b791cb7e00dd571242408e3912365
Anik Ghosh <anik.50cal@gmail.com>
2026-01-13 22:49:09 +0530

    Setup for Striver's Course

---
+++ Striver's Course/Recursion/README.md
@@ -0,0 +1,4 @@
+Tasks
+    - Print all subsequences of an array where sum == k
+    - Print the first valid subsequence for the previous problem, and nothing else
+    - Write mergesort and quicksort from scratch
---
+++ Striver's Course/Recursion/mergesort.py
@@ -0,0 +1,10 @@
+from testcases import TESTCASES
+def mergesort(l, r, array):
+    pass
+def merge(l1, l2, r1, r2):
+    pass
+if __name__ == "__main__":
+    for index, testcase in enumerate(TESTCASES):
+        print("=============================================================")
+        print(f"Testcase {index + 1}: {testcase}")
+        print("=============================================================")
